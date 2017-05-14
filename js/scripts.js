7de6eb0a19cba343e5f55a5e53b6c49e51bafb23
Adrian <tommazost@gmail.com>
2017-05-14 09:29:50 +0200

    new branch1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 var withButtonClass = document.getElementsByClassName('button');
 
 for (var i = 0; i < withButtonClass.lenght; i++); {
-	alert(withButtonClass.innertext);
+	alert('napis z przycisku :' +withButtonClass[0].innerText);
+	alert('napis z przycisku :' +withButtonClass[1].innerText);
+	alert('napis z przycisku :' +withButtonClass[2].innerText);
+	alert('napis z przycisku :' +withButtonClass[3].innerText);
 }
