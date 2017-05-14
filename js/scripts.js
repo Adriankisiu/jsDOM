94be13852adbb958c6ea74ddf49f1c3914140e56
Adrian <tommazost@gmail.com>
2017-05-14 20:32:41 +0200

    third corrections

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 var withButtonClass = document.getElementsByClassName('button');
 
-for (var i = 0; i < withButtonClass.lenght; i++); {
-	alert('napis z przycisku :' +withButtonClass[0].innerText);
-	alert('napis z przycisku :' +withButtonClass[1].innerText);
-	alert('napis z przycisku :' +withButtonClass[2].innerText);
-	alert('napis z przycisku :' +withButtonClass[3].innerText);
+for (var i = 0; i < withButtonClass.length; i++) {
+	alert('napis z przycisku :' + withButtonClass[i].innerText);
 }
